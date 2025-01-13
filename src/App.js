d6b97cec7b99cd24412f680a0ecdb925605deb4c
s24987 <s24987@pjwstk.edu.pl>
2025-01-14 00:05:38 +0100

    implement fetching user data

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './styles/App.css';
 
 import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom';
 import Home, {allBooksLoader} from "./components/Home";
-import Profile from "./components/Profile";
+import Profile, {profileLoader} from "./components/Profile";
 import MainLayout from "./components/MainLayout";
 import Library from "./components/Library";
 import Statistics from "./components/Statistics";
@@ -27,7 +27,7 @@ function App() {
             <Route path="/" element={<MainLayout isUserLoggedIn={isUserLoggedIn} logIn={logIn} logOut={logOut}/>}>
                 <Route index element={<Home isUserLoggedIn={isUserLoggedIn} isUserAdmin={isUserAdmin}/>}
                        loader={allBooksLoader}/>
-                <Route path={"profile"} element={<Profile/>}/>
+                <Route path={"profile"} element={<Profile/>} loader={profileLoader}/>
                 <Route path={"library"} element={<Library/>}/>
                 <Route path={"statistics"} element={<Statistics/>}/>
                 <Route path={"rankings"} element={<Rankings/>}/>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,15 +1,45 @@
 import React from "react";
-import {useOutletContext} from "react-router-dom";
+import {useLoaderData, useOutletContext} from "react-router-dom";
 
-const Home = () => {
+const Profile = () => {
     const [setHeaderTitle] = useOutletContext();
+    const userData = useLoaderData();
     setHeaderTitle("Profil użytkownika");
 
-    return (
-        <>
-            <h1>Witaj na profilu!</h1>
-        </>
-    )
+    // if no error message, display user data
+    if (typeof userData.message === 'undefined')
+        return (
+            <>
+                <h1>Witaj, {userData.username}!</h1>
+                <section>
+                    <b>Adres e-mail:</b> {userData.email}
+                    <br/>
+                    <b>Data urodzenia:</b> {new Date(userData.birthdate).toISOString().split('T')[0]}
+                </section>
+                <button>Edytuj profil</button>
+                <button className="delete btn-next">Usuń profil</button>
+            </>
+        )
+    else
+        return (
+            <h2>Nie jesteś zalogowany.</h2>
+        )
 };
 
-export default Home;
+export const profileLoader = async () => {
+    return fetch('http://localhost:8080/users', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        credentials: 'include',
+    })
+        .then(response => {
+            let res = response.json()
+            console.log(res);
+            return res;
+        })
+        .catch((error) => console.log(error));
+};
+
+export default Profile;
