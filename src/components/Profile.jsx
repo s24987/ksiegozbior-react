7815b06aff86ff240bdb04d4d57259273769880d
s24987 <s24987@pjwstk.edu.pl>
2025-01-14 13:18:46 +0100

    add edit view

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {useLoaderData, useOutletContext} from "react-router-dom";
 
 const Profile = () => {
@@ -6,21 +6,57 @@ const Profile = () => {
     const userData = useLoaderData();
     setHeaderTitle("Profil użytkownika");
 
+    const [email, setEmail] = useState(userData.email);
+    const [birthdate, setBirthdate] = useState(new Date(userData.birthdate).toISOString().split('T')[0]);
+    const [isEditView, setIsEditView] = useState(false);
+
+    const toggleEditView = () => {
+        setIsEditView(prevState => !prevState);
+    }
+
     // if no error message, display user data
-    if (typeof userData.message === 'undefined')
+    if (typeof userData.message === 'undefined') {
         return (
             <>
                 <h1>Witaj, {userData.username}!</h1>
                 <section>
                     <b>Adres e-mail:</b> {userData.email}
                     <br/>
+                    {isEditView && (
+                        <>
+                            <input type="email" id="input_email" name="email" className="dynamic-edit-input"
+                                   value={email} onChange={(e) => {
+                                setEmail(e.target.value)
+                            }}/>
+                            <br/>
+                        </>
+                    )}
+
                     <b>Data urodzenia:</b> {new Date(userData.birthdate).toISOString().split('T')[0]}
+                    <br/>
+                    {isEditView && (
+                        <>
+                            <input type="date" id="input_birthday" name="birthday" className="dynamic-edit-input"
+                                   value={birthdate} onChange={(e) => {
+                                setBirthdate(e.target.value)
+                            }}/>
+                            <br/>
+                        </>
+                    )}
+
                 </section>
-                <button>Edytuj profil</button>
-                <button className="delete btn-next">Usuń profil</button>
+                <button onClick={toggleEditView}>
+                    {isEditView? "Anuluj" : "Edytuj profil"}
+                </button>
+                {isEditView && (
+                    <button className="btn-next">Zapisz</button>
+                )}
+                {!isEditView && (
+                    <button className="delete btn-next">Usuń profil</button>
+                )}
             </>
         )
-    else
+    } else
         return (
             <h2>Nie jesteś zalogowany.</h2>
         )
