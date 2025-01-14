112d0a07eb4202f1e02bfc95d7a57ad2d0163d1d
s24987 <s24987@pjwstk.edu.pl>
2025-01-14 19:32:28 +0100

    implement log-out

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,11 +10,33 @@ const Header = ({
                     logIn,
                     logOut
                 }) => {
+    const handleLogout = async (e) => {
+        try {
+            const response = await fetch("http://localhost:8080/logout", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                credentials: 'include',
+            });
+
+            if (!response.ok) {
+                const errorData = await response.json();
+                console.log(errorData);
+                return;
+            }
+
+            logOut();
+        } catch (err) {
+            console.error("Błąd podczas wylogowywania:", err);
+        }
+    }
+
     return (
         <header id="main_header">
             <h1>{title}</h1>
             {isUserLoggedIn ? (
-                <button onClick={logOut}>Wyloguj się</button>
+                <button onClick={handleLogout}>Wyloguj się</button>
             ): (
                 <button onClick={openAccountModal}>Zaloguj się</button>
             )}
