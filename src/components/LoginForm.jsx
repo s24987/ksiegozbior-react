a70b3603527a021e72fbf276f4f3f98aa1419215
s24987 <s24987@pjwstk.edu.pl>
2025-01-18 21:26:48 +0100

    implement form validation with React Hook Form & Yup

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,12 +1,29 @@
 import {useState} from "react";
 import {parseErrors} from "../utils";
+import {useForm} from "react-hook-form";
+import {yupResolver} from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 const LoginForm = ({closeModal, logIn}) => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
 
-    const handleSubmit = async (e) => {
+    const validationSchema = yup.object().shape({
+        username: yup.string().min(3, "Nazwa użytkownika musi mieć co najmniej 3 znaki").required("Nazwa użytkownika jest wymagana"),
+        password: yup.string().min(6, "Hasło musi mieć co najmniej 6 znaków").required("Hasło jest wymagane"),
+    });
+
+    const {
+        register,
+        handleSubmit,
+        formState: {errors},
+    } = useForm({
+        resolver: yupResolver(validationSchema),
+    });
+
+
+    const onSubmit = async (e) => {
         e.preventDefault();
 
         try {
@@ -37,14 +54,21 @@ const LoginForm = ({closeModal, logIn}) => {
     return (
         <section>
             <h2>Logowanie użytkownika</h2>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <label htmlFor="input_username">Nazwa użytkownika</label>
-                <input type="text" id="input_username" name="username" onChange={(e) => setUsername(e.target.value)}/>
+                <input type="text" id="input_username" name="username" {...register("username")}
+                       onChange={(e) => setUsername(e.target.value)}/>
+                {errors.username && <p className="error-message">{errors.username.message}</p>}
+
                 <label htmlFor="input_password">Utwórz nowe hasło:</label>
-                <input type="password" id="input_password" name="password" onChange={(e) => setPassword(e.target.value)}/>
+                <input type="password" id="input_password" name="password"  {...register("password")}
+                       onChange={(e) => setPassword(e.target.value)}/>
+                {errors.password && <p className="error-message">{errors.password.message}</p>}
+
                 {error !== '' &&
                     <p id="error_summary">{error}</p>
                 }
+
                 <input
                     type="submit"
                     name="submit"
