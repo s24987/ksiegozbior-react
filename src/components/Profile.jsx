7c20f3effc9742eb04134a9efec6cac5c15e35be
s24987 <s24987@pjwstk.edu.pl>
2025-01-19 00:20:27 +0100

    implement form validation

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,11 +1,15 @@
-import React, {useState} from "react";
-import {useLoaderData, useOutletContext} from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import {useLoaderData, useOutletContext, useRevalidator} from "react-router-dom";
 import {parseErrors} from "../utils";
+import {useForm} from "react-hook-form";
+import {yupResolver} from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 const Profile = ({isUserLoggedIn, logOut}) => {
     const [setHeaderTitle] = useOutletContext();
     const userData = useLoaderData();
     setHeaderTitle("Profil użytkownika");
+    const revalidator = useRevalidator();
 
     const [email, setEmail] = useState(userData.email);
     const [birthdate, setBirthdate] = useState(userData.birthdate ? new Date(userData.birthdate).toISOString().split('T')[0] : null);
@@ -14,16 +18,41 @@ const Profile = ({isUserLoggedIn, logOut}) => {
     const [password, setPassword] = useState("");
     const [error, setError] = useState(null);
 
+    const validationSchema = yup.object().shape({
+        fullName: yup.string()
+            .min(3, "Imię i nazwisko musi mieć co najmniej 3 znaki")
+            .max(255, "Imię i nazwisko nie może mieć więcej niż 255 znaków")
+            .required("To pole jest wymagane"),
+        email: yup.string()
+            .email("Niepoprawny adres e-mail")
+            .max(255, "Adres e-mail nie może mieć więcej niż 255 znaków")
+            .required("Adres e-mail jest wymagany"),
+        password: yup.string()
+            .min(6, "Hasło musi mieć co najmniej 6 znaków")
+            .max(255, "Hasło nie może mieć więcej niż 255 znaków")
+            .required("Hasło jest wymagane"),
+        birthdate: yup.date()
+            .typeError("Data urodzenia musi być poprawną datą")
+            .required("Data urodzenia jest wymagana")
+    });
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        resolver: yupResolver(validationSchema),
+    });
+
     const toggleEditView = () => {
         setIsEditView(prevState => !prevState);
         if (!isEditView) {
             setError(null);
+            revalidator.revalidate();
         }
     }
 
-    const handleUserUpdate = async (e) => {
-        e.preventDefault();
-
+    const handleUserUpdate = async () => {
         const userUpdateData = {
             username: userData.username,
             fullName: fullName,
@@ -55,7 +84,11 @@ const Profile = ({isUserLoggedIn, logOut}) => {
         }
     }
 
-    const hadleUserDelete = async () => {
+    const onSubmit = (e) => {
+        handleUserUpdate();
+    }
+
+    const handleUserDelete = async () => {
         try {
             const response = await fetch("http://localhost:8080/users", {
                 method: 'DELETE',
@@ -82,16 +115,19 @@ const Profile = ({isUserLoggedIn, logOut}) => {
         return (
             <>
                 <h1>Witaj, {userData.username}!</h1>
+                <form onSubmit={handleSubmit(onSubmit)}>
                 <section>
                     <b>Imię i nazwisko:</b> {userData.fullName}
                     <br/>
                     {isEditView && (
                         <>
                             <input type="text" id="input_full_name" name="full_name" className="dynamic-edit-input"
-                                   value={fullName} onChange={(e) => {
+                                   value={fullName} {...register("fullName")}
+                                   onChange={(e) => {
                                 setFullName(e.target.value)
                             }}/>
                             <br/>
+                            {errors.fullName && <p className="error-message">{errors.fullName.message}</p>}
                         </>
                     )}
 
@@ -100,10 +136,11 @@ const Profile = ({isUserLoggedIn, logOut}) => {
                     {isEditView && (
                         <>
                             <input type="email" id="input_email" name="email" className="dynamic-edit-input"
-                                   value={email} onChange={(e) => {
+                                   value={email} {...register("email")} onChange={(e) => {
                                 setEmail(e.target.value)
                             }}/>
                             <br/>
+                            {errors.email && <p className="error-message">{errors.email.message}</p>}
                         </>
                     )}
 
@@ -112,18 +149,20 @@ const Profile = ({isUserLoggedIn, logOut}) => {
                     {isEditView && (
                         <>
                             <input type="date" id="input_birthday" name="birthday" className="dynamic-edit-input"
-                                   value={birthdate} onChange={(e) => {
+                                   value={birthdate} {...register("birthdate")} onChange={(e) => {
                                 setBirthdate(e.target.value)
                             }}/>
                             <br/>
+                            {errors.birthdate && <p className="error-message">{errors.birthdate.message}</p>}
                         </>
                     )}
 
                     {isEditView && (
                         <>
                             <label htmlFor="input_password">Nowe hasło:</label>
-                            <input type="password" id="input_password" name="password"
+                            <input type="password" id="input_password" name="password" {...register("password")}
                                    onChange={(e) => setPassword(e.target.value)}/>
+                            {errors.password && <p className="error-message">{errors.password.message}</p>}
                         </>
                     )}
 
@@ -131,15 +170,17 @@ const Profile = ({isUserLoggedIn, logOut}) => {
                         <p id="error_summary">{error}</p>
                     )}
                 </section>
+
                 <button onClick={toggleEditView}>
                     {isEditView ? "Anuluj" : "Edytuj profil"}
                 </button>
                 {isEditView && (
-                    <button className="btn-next" onClick={handleUserUpdate}>Zapisz</button>
+                    <button type="submit" className="btn-next">Zapisz</button>
                 )}
                 {!isEditView && (
-                    <button className="delete btn-next" onClick={hadleUserDelete}>Usuń profil</button>
+                    <button className="delete btn-next" onClick={handleUserDelete}>Usuń profil</button>
                 )}
+                </form>
             </>
         )
     } else
